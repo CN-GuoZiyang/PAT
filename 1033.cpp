14b6db5e348ba839cc3c015ac6b26d4916b11f49
Ziyang <guoziyang0033@gmail.com>
2020-02-25 17:29:16 +0800

    1033

--- 1033.cpp
+++ 1033.cpp
@@ -33,13 +33,47 @@ int main()
         printf("The maximum travel distance = 0.00");
         return 0;
     }
-    int cIndex = 0;
+    double cDis = 0.0, maxDis = 0.0, leftDis=0.0;
     double cPrice = stations[0].price;
     double totalPrice = 0.0;
     double cc = 0.0;
-    
-
-
+    while(cDis < d)
+    {
+        maxDis = cDis + cmax * davg;
+        double minPriceDis = 0.0, minPrice = 999999.9;
+        bool flag = false;
+        for(int i = 1; i <= n && stations[i].dis <= maxDis; i ++)
+        {
+            if(stations[i].dis <= cDis) continue;
+            if(stations[i].price < cPrice)
+            {
+                totalPrice += (stations[i].dis - cDis - leftDis) * cPrice / davg;
+                leftDis = 0.0;
+                cPrice = stations[i].price;
+                cDis = stations[i].dis;
+                flag = 1;
+                break;
+            }
+            if(stations[i].price < minPrice)
+            {
+                minPrice = stations[i].price;
+                minPriceDis = stations[i].dis;
+            }
+        }
+        if(flag == false && minPrice != 999999.9)
+        {
+            totalPrice += (cPrice * (cmax - leftDis/davg));
+            leftDis = cmax * davg - (minPriceDis - cDis);
+            cPrice = minPrice;
+            cDis = minPriceDis;
+        }
+        if(flag == 0 && minPrice == 999999.9)
+        {
+            cDis += cmax * davg;
+            printf("The maximum travel distance = %.2f", cDis);
+            return 0;
+        }
+    }
     printf("%.2lf", totalPrice);
     return 0;
 }
