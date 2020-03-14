20eb784c5821aa80dbfdc30d32585d82dcbf14f5
Ziyang <guoziyang0033@gmail.com>
2020-03-14 12:25:31 +0800

    1056

---
+++ 1057.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int n;
+    
+    scanf("%d", &n);
+    vector<int> stack;
+    for(int i = 0; i < n; i ++) {
+
+    }
+}
